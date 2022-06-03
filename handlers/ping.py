58eb96bd8fec8e24fadea4230f7e1e8310524751
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-04 01:12:45 +0530

    Create ping.py

---
+++ handlers/ping.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+@command(pattern="^.ping")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("pong")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("""
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱
+┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱╱
+┃╰━╯┃╭━━╮╭━━╮╭━━╮
+┃╭━━╯┃╭╮┃┃╭╮┃┃╭╮┃
+┃┃╱╱╱┃╰╯┃┃┃┃┃┃╰╯┃
+╰╯╱╱╱╰━━╯╰╯╰╯╰━╮┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯\n\n
+   MY MS → `{ms}`\n  !\n{}".format(ms))
