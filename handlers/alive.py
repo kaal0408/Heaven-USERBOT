63458792da4ab78fc9271bb16332286ac869769b
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-03 21:52:41 +0530

    Update alive.py

--- handlers/alive.py
+++ handlers/alive.py
@@ -6,7 +6,7 @@ from pyrogram import Client
 from main import ALIVE_PIC
  
 
-@Client.on_message(filters.command(["salive", "sawake"], [".", "!"]) & filters.user(SUDOERS))
+@Client.on_message(filters.command(["alive", "awake"], [".", "!"]) & filters.user(SUDOERS))
 async def alive(client: Client, e: Message):
     ids = 0
     try:
@@ -46,7 +46,7 @@ async def alive(client: Client, e: Message):
         Alive_msg = f"нεαvεη υsεявσт ιs αℓιvε🔥 \n\n"
         Alive_msg += f"◈ ━━━━━━ ◆ ━━━━━━ ◈ \n"
         Alive_msg += f"► vεяsιση : `Beta.0.1` \n"
-        Alive_msg += f"► ρүяσ vεяsιση : `1.4.15` \n"
+        Alive_msg += f"► ρүяσ vεяsιση : `2.0.5` \n"
         Alive_msg += f"► sυρρσят : [Jᴏɪɴ](https://t.me/Murat_30_God) \n"
         Alive_msg += f"◈ ━━━━━━ ◆ ━━━━━━ ◈ \n\n"
         await e.reply_photo(
@@ -64,60 +64,3 @@ async def alive(client: Client, e: Message):
         ),
     ) 
 
-@Client.on_message(filters.command(["alive", "awake"], [".", "!"]) &  filters.me)
-async def alive(client: Client, e: Message):
-    ids = 0
-    try:
-        if bot:
-            ids += 1
-        if bot1:
-            ids += 1
-        if bot2:
-            ids += 1
-        if bot3:
-            ids += 1
-        if bot4:
-            ids += 1
-        if bot5:
-            ids += 1
-        Alive_msg = f"нεαvεη υsεявσт ιs αℓιvε 🔥 \n\n"
-        Alive_msg += f"◈ ━━━━━━ ◆ ━━━━━━ ◈ \n"
-        Alive_msg += f"► vεяsιση : `Beta.0.1` \n"
-        Alive_msg += f"► ρүяσ vεяsιση : `{pyro_vr}` \n"
-        Alive_msg += f"► αcтιvε ι∂s : `{ids}` \n"
-        Alive_msg += f"► sυρρσят : [Jᴏɪɴ.](https://t.me/Murat_30_God) \n"
-        Alive_msg += f"◈ ━━━━━━ ◆ ━━━━━━ ◈ \n\n"
-        await e.reply_photo(
-        photo=ALIVE_PIC,
-        caption=Alive_msg,
-        reply_markup=InlineKeyboardMarkup(
-                [[
-                    InlineKeyboardButton(
-                        "• 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 •", url="https://t.me/Murat_30_God")
-                ], [
-                    InlineKeyboardButton(
-                        "• 𝐑𝐞𝐩𝐨 •", url="https://github.com/kaal0408/Heaven-USERBOT")
-                ]],
-        ),
-    ) 
-    except Exception as lol:         
-        Alive_msg = f"нεαvεη υsεявσт ιs αℓιvε🔥 \n\n"
-        Alive_msg += f"◈ ━━━━━━ ◆ ━━━━━━ ◈ \n"
-        Alive_msg += f"► vεяsιση : `Beta.0.1` \n"
-        Alive_msg += f"► ρүяσ vεяsιση : `1.4.15` \n"
-        Alive_msg += f"► sυρρσят : [Jᴏɪɴ](https://t.me/Murat_30_God) \n"
-        Alive_msg += f"◈ ━━━━━━ ◆ ━━━━━━ ◈ \n\n"
-        await e.reply_photo(
-        photo=ALIVE_PIC,
-        caption=Alive_msg,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("• Channel •", url="https://t.me/Murat_30_God"),
-                ],
-                [
-                    InlineKeyboardButton("• Repo •", url="https://github.com/kaal0408/Heaven-Userbot"),
-                ],
-            ],
-        ),
-    ) 
