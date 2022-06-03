8e34651ee04cc1865ca11194b5d6db93427954a9
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-04 01:16:51 +0530

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from pyrogram import Client
 from pyrogram import filters
 from main import *
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 @Client.on_message(filters.command(["ping", "Ping"], [".", "!"]) & filters.user(SUDOERS))
 async def _(event):
