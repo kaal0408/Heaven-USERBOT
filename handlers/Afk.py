474d4d1658d9e7e39fdd54162ce3521d37f3c809
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-04 13:12:48 +0530

    Update Afk.py

--- handlers/Afk.py
+++ handlers/Afk.py
@@ -1,5 +1,5 @@
 import time
-
+from main import SUDOERS
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from helpers.SQL import DB_AVAILABLE
