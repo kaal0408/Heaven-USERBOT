74dce9c2ca8cae045930411dbc009c6a4e6ba211
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-04 19:23:57 +0530

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -6,6 +6,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 ms = "Heaven user"
 
+
+@Client.on_message(filters.command(["ping", "Ping"], [".", "!"]) & filters.me)
 @Client.on_message(filters.command(["ping", "Ping"], [".", "!"]) & filters.user(SUDOERS))
 async def ping(_, message: Message):
     await message.reply_text(f"""⭐ **
